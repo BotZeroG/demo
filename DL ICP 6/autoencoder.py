b780c4e5426b0a56d8bba17d52b16dac8c8f6bc0
BotZeroG <grhv26@umsystem.edu>
2020-07-23 14:55:33 -0500

    Create autoencoder.py

---
+++ DL ICP 6/autoencoder.py
@@ -0,0 +1,82 @@
+import matplotlib
+from keras.layers import Input, Dense
+from keras.models import Model
+from keras.datasets import fashion_mnist
+import numpy as np
+import matplotlib.pyplot as plt
+matplotlib.use('TkAgg')
+# this is the size of our encoded representations
+encoding_dim = 32  # 32 floats -> compression of factor 24.5, assuming the input is 784 floats
+
+# this is our input placeholder
+input_img = Input(shape=(784,))
+# "encoded" is the encoded representation of the input
+encoded = Dense(encoding_dim, activation='relu')(input_img)
+
+# hidden layer
+hidden = Dense(32, activation='relu')(encoded)
+
+# "decoded" is the lossy reconstruction of the input
+decoded = Dense(784, activation='sigmoid')(hidden)
+# this model maps an input to its reconstruction
+autoencoder = Model(input_img, decoded)
+# maps an input to encoded representation
+encoder = Model(input_img, encoded)
+# placeholder for an encoded input
+encoded_input = Input(shape=(encoding_dim,))
+# retrieve the layer of the autoencoder
+decoder_layer = autoencoder.layers[-1]
+# create the decoder model
+decoder = Model(encoded_input, decoder_layer(encoded_input))
+# this model maps an input to its encoded representation
+autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy', metrics=['accuracy'])
+
+(x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
+x_train = x_train.astype('float32') / 255.
+x_test = x_test.astype('float32') / 255.
+x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
+x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
+
+autoencoder.fit(x_train, x_train,
+                epochs=5,
+                batch_size=256,
+                shuffle=True,
+                validation_data=(x_test, x_test))
+
+encoded_imgs = encoder.predict(x_test)
+decoded_imgs = decoder.predict(encoded_imgs)
+
+prediction = autoencoder.predict(x_test[1].reshape(1, 784))
+
+history = autoencoder.fit(x_train, x_train, batch_size=256, epochs=10, verbose=1,
+                          validation_data=(x_test, x_test))
+
+plt.imshow(x_test[1].reshape(28, 28))
+plt.show()
+
+plt.imshow(encoded_imgs[1].reshape(16, 2))
+plt.show()
+
+plt.imshow(prediction.reshape(28, 28))
+plt.show()
+
+# list all data in history
+print(history.history.keys())
+# summarize history for accuracy
+
+# summarize history for loss
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
+
+plt.plot(history.history['accuracy'])
+plt.plot(history.history['val_accuracy'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
